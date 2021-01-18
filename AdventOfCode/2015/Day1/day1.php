ca3383639a5897fdb6fe0da717889c6b5fef228f
synstealth <synstealth@gmail.com>
2021-01-18 16:31:19 -0500

    Update day1.php

--- AdventOfCode/2015/Day1/day1.php
+++ AdventOfCode/2015/Day1/day1.php
@@ -6,15 +6,12 @@ $map = str_split($output);
 
 $floor=0;$base=false;$level=0;
 foreach($map as $chr){
-	$dir = str_split($chr,1);
-	foreach($dir as $d){
-		if(!$base){$level++;}
-		switch($d){
-			case '(': $floor++;	break;
-			case ')': $floor--; break;
-		}
-		if($floor <= -1){$base=true;}
+	if(!$base){$level++;}
+	switch($chr){
+		case '(': $floor++;	break;
+		case ')': $floor--; break;
 	}
+	if($floor <= -1){$base=true;}
 }
 echo 'You are now at Floor: '.$floor.' and the first basement is: '.$level.'<br />';
 ?>
