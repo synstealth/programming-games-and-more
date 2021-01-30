4964c3d6208b8a080da298b0fd404e4ec8b88e07
synstealth <synstealth@gmail.com>
2021-01-30 12:07:41 -0500

    Update prog2.php

--- EnigmaGroup/programming/prog2.php
+++ EnigmaGroup/programming/prog2.php
@@ -4,28 +4,21 @@
 #############################################################
 $Url = "http://www.enigmagroup.org/forums/login2/";
 $login_email = 'synstealth';
-$login_pass = 'SevenMGTEbug007';
+$login_pass = 'xxxxxxx';
 #############################################################
-
-// is curl installed?
-if (!function_exists('curl_init')){ die('CURL is not installed!');}
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $Url);
 curl_setopt($ch, CURLOPT_REFERER, "http://www.enigmagroup.org");
 curl_setopt($ch, CURLOPT_USERAGENT, "MozillaXYZ/1.0");
 curl_setopt($ch, CURLOPT_HEADER, 0); 
-
-// post the login data (string)
-curl_setopt($ch, CURLOPT_POSTFIELDS,'user='.urlencode($login_email).'&passwrd='.urlencode($login_pass).'&cookieneverexp=on&hash_password=a7f9e74c5f2dcdd07998c42007fac4a3');
+curl_setopt($ch, CURLOPT_POSTFIELDS,'user='.urlencode($login_email).'&passwrd='.urlencode($login_pass).'&cookieneverexp=on&hash_password=xxxxxxxxxx');
 curl_setopt($ch, CURLOPT_POST, 1);
-
-// should curl return or print the data? 1 = return, 0 = print
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
 curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
-$page = curl_exec($ch);  // done logged in //
+$page = curl_exec($ch); 
 
 ##########################################################################################################################
 
@@ -57,7 +50,6 @@ $hash = $matches[2][3];
 curl_setopt($ch, CURLOPT_POSTFIELDS,'answer='.$answer.'&E%5Bnumber%5D='.$Enumber.'&E%5Btime%5D='.$Etime.'&hash='.$hash.'&submit=Submit+Answer');
 
 
-// render the post data //
 echo curl_exec($ch);
 curl_close($ch);
 ?>
