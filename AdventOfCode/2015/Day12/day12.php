825b30633695db3cb525836c67ceeb77fd15e76e
synstealth <synstealth@gmail.com>
2021-02-01 16:04:45 -0500

    Update day12.php

--- AdventOfCode/2015/Day12/day12.php
+++ AdventOfCode/2015/Day12/day12.php
@@ -12,25 +12,4 @@ foreach($m[0] as $i){ $total += (int)$i; }
 
 echo 'There are a total of: '. $total.' numbers.<br />';
 
-function jsonTotal($basket=array(),$num){
-    $jtotal=0;
-    foreach($num as $line){
-        if(is_array($line)){
-            $jtotal += jsonTotal($basket,$line);
-        }elseif(is_object($line)){
-            $error=false;
-            foreach($line as $n){
-                if(is_string($n) && in_array((string)$n,$basket)){
-                    $error=true;    break;
-                }
-            }
-            if(!$error){ $jtotal += jsonTotal($basket,$line);}
-        }elseif(is_numeric($line)){    $jtotal += $num; }
-    }
-    return $jtotal;
-}
-
-$json = json_decode($map[0]);
-echo 'There are a total of '.jsonTotal(array("red"),$json).' reds.<br />';
-
 ?>
