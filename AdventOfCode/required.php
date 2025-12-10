ad70bb85ad4beda829f07d3a94f6b0e294507753
Dustin Robinson <synstealth@gmail.com>
2025-12-10 13:58:11 -0600

    Update required.php
    session cookie hidden for privacy

--- AdventOfCode/required.php
+++ AdventOfCode/required.php
@@ -1,1 +1,18 @@
-
+<?php
+function loadCookies($day){
+	$session = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+	$url = "https://adventofcode.com/2025/day/".$day."/input";
+	$options = [
+		"http" => [
+			"method" => "GET",
+			"header" => "Cookie: session={$session}\r\n" . "User-Agent: synstealth@gmail.com\r\n"
+		]
+	];
+	
+	$context = stream_context_create($options);
+	$input = file_get_contents($url, false, $context);
+	if ($input === false){die("Failed to fetch Advent of Code input.");}
+	
+	return $input;
+}
+?>
